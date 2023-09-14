e1541143967de93692ab9a3c11ef2b0442d7ca49
murilobispo <murilooliveirabispo90@gmail.com>
2023-09-14 00:42:41 -0300

    Tela de SingUp e SignIn feitas, agora compartilhando da mesma folha de estilo, dois logins ja registrados foram adicionados em Banco de Dados/UserCredentials.js

---
+++ Root Folder/Banco de dados/userCredentials.js
@@ -0,0 +1,34 @@
+class User {
+    constructor(nome, telefone, email, senha) {
+        this.nome = nome
+        this.telefone = telefone
+        this.email = email
+        this.senha = senha
+    }
+}
+const u00000001 = new User('Oregon', '(85) 98989-8989','oregonstudios@gmail.com','amolivros123')
+const u00000002 = new User('Roger', '(85) 99898-9898','profroger@gmail.com','amolivros123')
+
+const inputEmail    = document.querySelector(".auth-email-input");
+const inputPassword = document.querySelector(".auth-password-input");
+const formLogin = document.querySelector(".form-login");
+
+let allUsers = { u00000001, u00000002}
+
+function signInAuth(email, senha){
+    const usuarioEncontrado = Object.values(allUsers).find(
+        user => user.email === email && user.senha === senha
+    );
+    if (usuarioEncontrado) {
+        alert("Login bem-sucedido!");
+    } else {
+        alert("Credenciais inválidas. Tente novamente.");
+    }
+}
+    
+formLogin.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const email = inputEmail.value;
+    const senha = inputPassword.value;
+    signInAuth(email, senha);
+});
