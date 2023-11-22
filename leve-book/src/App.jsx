37a2ec4ed73d190c33fdeba098624f733a54e829
murilobispo <murilooliveirabispo90@gmail.com>
2023-11-22 20:20:51 -0300

    Melhoria nas rotas

---
+++ leve-book/src/App.jsx
@@ -0,0 +1,33 @@
+// React Router
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// Paginas
+import Home from 'pages/Home';
+import Login from 'pages/Login';
+import Favoritos from 'pages/Favoritos'
+import Livros from 'pages/Livros';
+// Componentes
+import LivrosConfig from './routes/livrosConfig.json'
+
+
+export default function App() {
+
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='login' element={<Login />} />
+          <Route path='favoritos' element={<Favoritos />} />
+          
+          <Route path='livros' element={<Livros/>}>
+            {LivrosConfig.map( rota => (
+              <Route path={rota.path}  element={<Livros />} />
+            ))}
+          </Route>
+
+          <Route path='*' element={<h1>Página não encontrada</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
