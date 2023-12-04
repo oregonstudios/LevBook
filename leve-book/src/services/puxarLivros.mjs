77110574627c5b95b444cbad55af31ca131aaf41
franciscoweverton <francisco.weverton.oliveira62@aluno.ifce.edu.br>
2023-12-03 23:45:04 -0300

    trazendo os dados dos livros do firebase

--- leve-book/src/services/puxarLivros.mjs
+++ leve-book/src/services/puxarLivros.mjs
@@ -1,6 +1,5 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, getDocs } from 'firebase/firestore';
-import fs from 'fs/promises';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCcv4PdY_YZH_wWjt3T7_lyRGhX3wrh6Z0",
@@ -15,39 +14,47 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 async function obterLivrosPorColecao(colecao) {
-  const todosLivros = [];
+  const todosLivros = {
+    fantasia: [],
+    terror: [],
+    receitas: [],  // Você precisa adaptar os nomes dos gêneros conforme as suas coleções
+    religiao: [],
+    romances: [],
+    ficcao: []
+  };
 
   for (const nomeColecao of colecao) {
     const snapshot = await getDocs(collection(db, nomeColecao));
 
     const livrosColecao = snapshot.docs.map((doc) => {
       const livroData = doc.data();
-      const livroFormatado = Object.keys(livroData).reduce((acc, key) => {
-        acc[key.toLowerCase()] = livroData[key];
-        return acc;
-      }, {});
-
-      return {
-        id: doc.id,
-        ...livroFormatado,
+      const livroFormatado = {
+        imagem: "", // Certifique-se de definir a imagem correta aqui
+        ...livroData,
+        genero: nomeColecao.toLowerCase(),  // Adiciona o gênero
       };
+
+      return livroFormatado;
     });
 
-    todosLivros.push(...livrosColecao);
+    todosLivros[nomeColecao.toLowerCase()] = livrosColecao;
   }
 
   return todosLivros;
 }
 
+// Exemplo de uso
 async function main() {
-  const colecoes = ['Fantasia', 'Terror', 'Culinária', 'Religião', 'Ficção', 'Romances'];
+  const colecoes = ['Fantasia', 'Terror', 'Receitas', 'Religião', 'Romances', 'Ficção'];
   const todosLivros = await obterLivrosPorColecao(colecoes);
-  const jsonLivros = JSON.stringify(todosLivros, null, 2);
-  const caminhoArquivoJSON = 'livros.json';
 
-  await fs.writeFile(caminhoArquivoJSON, jsonLivros);
-
-  console.log(`Arquivo JSON gerado com sucesso: ${caminhoArquivoJSON}`);
+  // Agora você pode usar os arrays como desejar
+  console.log('Dados de Fantasia:', todosLivros.fantasia);
+  console.log('Dados de Terror:', todosLivros.terror);
+  console.log('Dados de Receitas:', todosLivros.receitas);
+  console.log('Dados de Religião:', todosLivros.religiao);
+  console.log('Dados de Romances:', todosLivros.romances);
+  console.log('Dados de Ficção:', todosLivros.ficcao);
 }
 
 main();
