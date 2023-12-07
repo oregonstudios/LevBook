04ee4171c5d6a3be8a80ffb847a877e8cc2e6287
murilobispo <murilooliveirabispo90@gmail.com>
2023-12-06 23:27:18 -0300

    Finalizando a tela de Livros de de suas respectivas Categorias

--- leve-book/src/pages/Livros.jsx
+++ leve-book/src/pages/Livros.jsx
@@ -5,6 +5,9 @@ import styled from "styled-components"
 import { useLocation, Link } from "react-router-dom"
 import { obterLivrosContent } from "../scripts/LivrosContent"
 import Logo from '../images/logo.png'
+import { LuFilter } from "react-icons/lu";
+import livrosConfig from '../routes/livrosConfig.json'
+
 
 const LivrosBackground = styled.div`
   width: 100%;
@@ -18,29 +21,75 @@ const LivrosWrapper = styled.main`
   box-sizing: border-box;
   display: flex;
   gap: 10px;
+  justify-content: space-between;
+`
+const LivrosMiniHeader = styled.div`
+  font-family: var(--font-main);
+  width: 1250px;
+  margin: 0 auto;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: space-between;
+  text-align: center;
+  align-items: center;
+  border-bottom: 1px solid #b8b8b8;
+  `
+const LivrosMiniHeaderSection = styled.div`
+  display: flex;
+  text-align: center;
+  align-items: center;
+  gap: 20px;
+  margin: 20px 0;
 `
 const FilterBar = styled.section`
   align-items: center;
   display: flex;
   flex-direction: column;
   align-items: start;
-  width: 300px;
+  width: 17%;
   gap: 20px;
-`
+  box-sizing: border-box;
+  font-family: var(--font-main);
 
+`
+const FilterHeader = styled.div`
+  display: flex;
+  gap: 5px;
+  align-items: center;
+  text-align: center;
+  width: 90%;
+  padding: 0 0 20px 0;
+  border-bottom: 1px solid #b8b8b8;
+
+`
+const FilterTitle = styled.p`
+`
 const CategoriaAtual = styled.h3`
   font-size:26px;
   font-weight: bold;
+  font-family: var(--font-secundaria);
+
 `
 const NavList = styled.ol`
   display: flex;
+  font-size: 15px;
+  gap: 5px;
+  font-family: var(--font-secundaria);
+  color: #5f5f5f;
 `
-const LinkItem = styled.li`
-    
+const LinkItem = styled(Link)`
+  text-decoration: none;
+  color:#5f5f5f;
+  &:hover{
+    color: var(--cor-secundaria);
+  }
 `
 
 const NumeroDeProdutos = styled.p`
-  font-size: 16px;
+  font-size: 15px;
+  font-family: var(--font-secundaria);
+  color:#5f5f5f;
+
 `
 
 const FilterOptions = styled.section`
@@ -49,20 +98,43 @@ const FilterOptions = styled.section`
   gap: 10px;
 `
 const FilterType = styled.p`
-  
-  `
+  font-size: 18px;
+
+`
 const FilterList = styled.ul`
-  
-  `
-const FilterListItem = styled.li`
+  display: flex;
+  flex-direction: column;
+  padding: 0 0 0 10px;
+  gap: 5px;
+
+`
+const FilterListItem = styled(Link)`
+  font-size: 16px;
+  text-decoration: none;
+  color: black;
+  &:hover{
+    color:var(--cor-secundaria)
+  }
 `
 const ResultsList = styled.ul`
   display: grid;
   grid-template-columns: repeat(5, 1fr);
-  grid-column-gap: 20px;
+  grid-column-gap: 25px;
   grid-row-gap: 30px; 
   padding: 0 auto; 
+  width: 83%;
+  box-sizing: border-box;
+
 `
+const ResultsItemImagemWrapper = styled.div`
+  height: 67%;
+  display: flex;
+  justify-content: center;
+  background-color: #dfdfdf;
+  width: 100%;
+  border-bottom: 3px solid #a1a1a1;
+  `
+
 const ResultsItem = styled.li`
   width: 185px;
   height: 330px;
@@ -79,30 +151,25 @@ const ResultsItem = styled.li`
   justify-content: space-between;
   &:hover {
     box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
+    ${ResultsItemImagemWrapper} {
+      border-bottom: 3px solid var(--cor-secundaria);
+    }
   }
 `
-const ResultsItemImagemWrapper = styled.div`
-  height: 67%;
-  display: flex;
-  justify-content: center;
-  background-color: #dfdfdf;
-  width: 100%;
-  border-bottom: 3px solid #a1a1a1;
 
-`
 const ResultsItemImagem = styled.img`
   height: 100%;
   align-self: center;
 `
 const ResultsItemTituloWrapper = styled.section`
   display: flex;
-  flex-direction: column;
   align-items: center;
   height: 11%;
   text-align: center;
   padding: auto 0;
   overflow: hidden;
   padding: 0 4px;
+  justify-content: center;
 `
 const ResultsItemTitulo = styled.p`
   font-size: 18px;
@@ -134,13 +201,22 @@ const ResultsItemPreco = styled.p`
   cursor: pointer;
   transition: all 0.3s cubic-bezier(.25,.8,.25,1);
   margin: 0 10px;
+  background-color: #eceaea;
+
   &:hover{
     background-color: var(--cor-primaria);
     color: #fff;
   }
 `
 export default function Livros(){
-
+    const verificaPathname = nomeLink =>{
+      if (nomeLink == "home"){
+        return "/"
+      }
+      if (nomeLink == "livros"){
+        return "/livros"
+      }
+    }
     const location = useLocation();
     const livrosContent = obterLivrosContent(location.pathname)
 
@@ -149,36 +225,39 @@ export default function Livros(){
         <Header/>
         <HeaderSection/>
         <LivrosBackground>
+          <LivrosMiniHeader>
+            <LivrosMiniHeaderSection>
+              <CategoriaAtual>{livrosContent.categoriaAtual}</CategoriaAtual>
+              <NavList>
+                {livrosContent.links.map((link, index, array) => [
+                  <LinkItem to={verificaPathname(link.toLowerCase())}>{link}</LinkItem>,
+                  index < array.length - 1 && ' > ',
+                ])}
+              </NavList>
+
+
+            </LivrosMiniHeaderSection>
+            <NumeroDeProdutos>{livrosContent.livros.length} Resultados</NumeroDeProdutos>
+
+          </LivrosMiniHeader>
 
           <LivrosWrapper>
+
               <FilterBar>
-                  <NavList>
-                    {livrosContent.links.map( link => (
-                      <LinkItem>{link} </LinkItem>
-                    ))}
-                  </NavList>
-                  <div>
-                    <CategoriaAtual>{livrosContent.categoriaAtual}</CategoriaAtual>
-                    <NumeroDeProdutos>{livrosContent.livros.length} Resultados</NumeroDeProdutos>
-                  </div>
-
-
+                <FilterHeader>
+                  <LuFilter/>
+                  <FilterTitle>Filtros</FilterTitle>
+                </FilterHeader>
                 <FilterOptions>
                   <FilterType>Categorias</FilterType>
                   <FilterList>
-                    <FilterListItem>Fantasia</FilterListItem>
-                    <FilterListItem>Ficção</FilterListItem>
-                    <FilterListItem>Receitas</FilterListItem>
-                    <FilterListItem>Religião</FilterListItem>
-                    <FilterListItem>Romances</FilterListItem>
-                    <FilterListItem>Terror</FilterListItem>
+                    {livrosConfig.map( rota => (
+                      <FilterListItem to={rota.path}>{rota.categoria}</FilterListItem>
+                    ))}
                   </FilterList>
                 </FilterOptions>
-
-
-              </FilterBar>
-              <ResultsList>
-
+            </FilterBar>
+            <ResultsList>
               {livrosContent.livros.map( livro => (
                 <ResultsItem>
                   <ResultsItemImagemWrapper>
@@ -195,13 +274,14 @@ export default function Livros(){
                   <ResultsItemAutorWrapper>
                     <ResultsItemAutor>{livro.Autor}</ResultsItemAutor>
                   </ResultsItemAutorWrapper>
-                  
+                
                   <ResultsItemPrecoWrapper>
                     <ResultsItemPreco>A partir de R$ {livro.Preço}</ResultsItemPreco>
                   </ResultsItemPrecoWrapper>
                 </ResultsItem>
               ))}
-              </ResultsList>
+            </ResultsList>
+
           </LivrosWrapper>
         </LivrosBackground>
         <Footer/>
